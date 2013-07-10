cc6271186592e25bda6d36c30230ab3fd6e957c7
Pundarik <pundarickr@gmail.com>
2013-07-10 17:56:27 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var fs = require('fs');
+//var buffer = new buffer ();
+var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');  
+response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
